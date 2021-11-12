2dd40f71581ec00366bb8f36a68de2162535d485
Maria22 <m.ushakova22@gmail.com>
2021-11-12 15:50:29 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import Home from './pages/Home/Home';
+import Example from './pages/Example/Example';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      Привет
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/example' exact element={<Example />} />
+    </Routes>
   );
 }
 
