f94795dc5210a5aa0d322af96e84f397d40e68e1
Maria22 <m.ushakova22@gmail.com>
2021-11-12 16:22:41 +0100

    Add global styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import Home from './pages/Home/Home';
-import Example from './pages/Example/Example';
-import { Routes, Route } from 'react-router-dom';
+import React from 'react'
+import Header from './global/Header/Header'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Example from './pages/Example/Example'
+
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/example' exact element={<Example />} />
-    </Routes>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/example' exact element={<Example />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/global/Header/Header.js
+++ src/global/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import style from './Header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className={style.header}>
+      Header
     </div>
   )
 }
