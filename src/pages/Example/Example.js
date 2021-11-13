99ed2ef93a50a861bb8f7f5e32efd41381ae1b8c
Maria22 <m.ushakova22@gmail.com>
2021-11-13 20:33:25 +0100

    Add style to main parts

--- src/pages/Example/Example.js
+++ src/pages/Example/Example.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import './../../styles/index.scss'
+import Foto from './../../images/foto.jpeg'
+import style from './Example.module.scss'
 
 const Example = () => {
   return (
     <div>
-      Example1
+      <h1>Пример</h1>
+      <h2>Картинка с котом</h2>
+      <div className={style.exampleContainer} >
+        <img src={Foto} alt="Фото сотрудника" className={style.exampleImage}/>
+        <button>Загрузить другую</button>
+      </div>
+
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import './../../styles/index.scss'
+import Foto from './../../images/foto.jpeg'
+import style from './Home.module.scss'
 
 const Home = () => {
   return (
-    <div>
+    <>
       <h1>Обо мне</h1>
-    </div>
+      <h2>Мария Ушакова</h2>
+      <div className={style.homeContainer}>
+        <div className={style.homeDescription}>
+          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+            deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident,
+            similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+            rerum facilis est et expedita distinctio.
+          </p>
+          <button>Смотреть работу</button>
+        </div>
+        <img src={Foto} alt="Фото сотрудника" className={style.homeImage} />
+      </div>
+    </>
   )
 }
 
